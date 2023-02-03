5c4e2722d5557355e4df81fbb01220ca1fbf3d1e
Seattleyrz <64921652+Seattleyrz@users.noreply.github.com>
2023-02-03 01:31:48 -0600

    Update happyFetch.py

--- happyFetch.py
+++ happyFetch.py
@@ -7,11 +7,12 @@ def happyFetch(points_spent, path):
   df = df.sort_values(by = 'timestamp', ignore_index = True)
   total_points = -abs(points_spent)
   negative_sum = 0
+#   for loop to get all negative values and sum them up
   for key, value in df.get('points').iteritems():
     if(value < 0):
       negative_sum += value
       df.at[key, 'points'] = 0
-  
+#   for loop to balance points according to the order of timestamp
   for key, value in df.get('points').iteritems():
     if(value < abs(negative_sum)):
       df.at[key, 'points'] = 0
@@ -23,9 +24,10 @@ def happyFetch(points_spent, path):
   
   df = df.sort_values(by = 'timestamp', ignore_index = True)
   points_sum = sum(df.get('points'))
+#   edge case: if payers are running out of points
   if(points_sum < points_spent):
       return("Running out of points")
-  
+#   for loop to use up all the points given
   for key, value in df.get('points').iteritems():
     if(value != 0):
       if(value < abs(total_points)):
